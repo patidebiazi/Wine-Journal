6310eac62cdb89a1a1616ba1c32bec7393abc46b
Patricia Debiazi <patriciagdebiazi@hotmail.com>
2024-05-10 16:05:13 +0100

    Initial Commit

---
+++ Wine Journal/Wine_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wine_JournalApp.swift
+//  Wine Journal
+//
+//  Created by Patricia D on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Wine_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
